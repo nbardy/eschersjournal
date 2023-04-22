fc046ff65bcfcc8cfd305593e6e3c342bd87d2d6
Nicholas Bardy <nicholasbardy@gmail.com>
2023-04-22 15:46:09 -0700

    summary

--- server/src/summaries.py
+++ server/src/summaries.py
@@ -13,19 +13,50 @@ HEADERS = {
 }
 
 
-def _process_summary(relevant_files, query_request: str):
-    chunks = _break_down_into_chunks(relevant_files)
+def load_all_files():
+    # Replace this function with the actual implementation to load all files from the desired source
+    all_files = [
+        {"name": "File 1", "filepath": "path/to/file1", "Summary": "File 1 content."},
+        {"name": "File 2", "filepath": "path/to/file2", "Summary": "File 2 content."},
+        {"name": "File 3", "filepath": "path/to/file3", "Summary": "File 3 content."},
+    ]
+    return all_files
+
+
+def generate_summary(query_request: str):
+    all_files = load_all_files()
+    file_summaries = {
+        file["name"]: f"{file['name']} summary: {file['Summary'][:100]}" for file in all_files}
 
     conversation_history = []
-
     system_message = f"{query_request}. You are a helpful assistant that will process documents, build an outline, and generate a summary of the current results."
 
-    for chunk_id, chunk in chunks.items():
+    summaries_chunks = _break_down_into_chunks(
+        file_summaries, max_context_size=4096, prompt_space=200)
+
+    relevant_file_ids = []
+
+    for chunk_id, chunk in summaries_chunks.items():
+        user_message = f"Based on the following summaries, decide which files are relevant:\n\n" + \
+            "\n".join(chunk.values())
+        relevant_files_str = ask_chatgpt_conversation(
+            conversation_history, system_message, user_message)
+        # Assuming the response contains comma-separated file names
+        relevant_file_ids.extend(relevant_files_str.split(", "))
+
+    relevant_files = {
+        file["name"]: file for file in all_files if file["name"] in relevant_file_ids}
+
+    content_chunks = _break_down_into_chunks(
+        {file["name"]: file["Summary"] for file in relevant_files.values()}, max_context_size=4096, prompt_space=200)
+
+    system_message = "Continue processing documents and updating the outline and summary of the current results."
+
+    for chunk_id, chunk in content_chunks.items():
         user_message = f"Process the document '{chunk_id}'. {chunk}"
         assistant_response = ask_chatgpt_conversation(
             conversation_history, system_message, user_message)
 
-        system_message = "Continue processing documents and updating the outline and summary of the current results."
         user_message = f"Based on the information from '{chunk_id}', update the outline and summary of the current results."
         assistant_response = ask_chatgpt_conversation(
             conversation_history, system_message, user_message)
